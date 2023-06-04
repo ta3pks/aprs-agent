a67c14420684de3a3204b972decf38529d117bee
nikos efthias <nikos@mugsoft.io>
2023-06-05 02:26:19 +0300

    add TCPIP to the path

--- src/extensions/fixed_beacon.rs
+++ src/extensions/fixed_beacon.rs
@@ -79,7 +79,7 @@ impl FixedBeacon {
         //TA3PKS-7>APAT81-15,WIDE1-1,WIDE2-2,qAR,TA3ML-1:!3800.00N/02700.00E>/A=000000ta3pks@mugsoft.io +905418622094 433.500 QRV
         let cfg = &crate::Config::get().extensions.fixed_beacon;
         let package = format!(
-            "{ssid}>AP4GNT,qAC,APRSAGENT:!{lat}{symbol_table}{lon}{symbol}{comment}\n",
+            "{ssid}>AP4GNT,TCPID*,qAC,APRSAGENT:!{lat}{symbol_table}{lon}{symbol}{comment}\n",
             ssid = cfg.ssid.to_uppercase(),
             lat = cfg.lat,
             symbol_table = cfg.symbol_table,
